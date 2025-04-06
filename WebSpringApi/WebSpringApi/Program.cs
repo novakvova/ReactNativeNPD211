907acdc5079a0c6bd401deaec8376c0f733742f8
novakvova <novakvova@gmail>
2025-04-06 14:53:15 +0300

    Add Image Service

--- WebSpringApi/WebSpringApi/Program.cs
+++ WebSpringApi/WebSpringApi/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using WebSpringApi.Abstract;
 using WebSpringApi.Data;
+using WebSpringApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<WebSpringDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("WebSpringConnection")));
+
+builder.Services.AddScoped<IImageService, ImageService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
