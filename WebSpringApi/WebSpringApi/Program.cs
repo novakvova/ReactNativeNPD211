8de2b82764dd6b9cbc8c6d7f88d7a25ce3a20a8a
novakvova <novakvova@gmail>
2025-04-06 15:47:18 +0300

    Add Json Data for Seed

--- WebSpringApi/WebSpringApi/Program.cs
+++ WebSpringApi/WebSpringApi/Program.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebSpringApi.Abstract;
 using WebSpringApi.Data;
+using WebSpringApi.Data.Entities.Identity;
 using WebSpringApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<WebSpringDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("WebSpringConnection")));
+
+builder.Services.AddIdentity<UserEntity, RoleEntity>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 5;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+}).AddEntityFrameworkStores<WebSpringDbContext>().AddDefaultTokenProviders();
 
 builder.Services.AddScoped<IImageService, ImageService>();
 
