5c5a5b2cb10196e9c65266300b441a8e3a1e9538
novakvova <novakvova@gmail>
2025-05-04 14:29:56 +0300

    Is good View categories

---
+++ WebSpringApi/WebSpringApi/Mapper/CategoryMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using WebSpringApi.Data.Entities;
+using WebSpringApi.Models.Category;
+
+namespace WebSpringApi.Mapper;
+
+public class CategoryMapper : Profile
+{
+    public CategoryMapper()
+    {
+        CreateMap<CategoryEntity, CategoryItemViewModel>();
+    }
+}
