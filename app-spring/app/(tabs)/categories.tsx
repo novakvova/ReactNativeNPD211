ab56ad6848712c6785c6a83a2ed7ba39c399abd0
novakvova <novakvova@gmail>
2025-05-11 16:12:25 +0300

    View List categories

--- app-spring/app/(tabs)/categories.tsx
+++ app-spring/app/(tabs)/categories.tsx
@@ -1,68 +1,50 @@
-import React from "react";
-import {
-    SafeAreaView,
-    StyleSheet,
-    ScrollView,
-    View,
-    Text,
-    KeyboardAvoidingView,
-    Platform,
-    Dimensions
-} from "react-native";
-import {SafeAreaProvider} from "react-native-safe-area-context";
-import {useAppSelector} from "@/store";
+import {View, Text, StyleSheet, FlatList} from "react-native";
+import {useRouter} from "expo-router";
+import {useAppDispatch, useAppSelector} from "@/store";
 import {useGetCategoriesQuery} from "@/services/categoriesService";
-import {isLoading} from "expo-font";
-
+import LoadingOverlay from "@/components/LoadingOverlay";
+import CategoryCard from "@/components/category/CategoryCard";
 
 
 const CategoriesScreen = () => {
-    const windowWidth = Dimensions.get('window').width;
+    const dispatch = useAppDispatch();
+    const router = useRouter();
 
     const token = useAppSelector((state) => state.auth.token);
 
-    const {data, isLoading, error} = useGetCategoriesQuery(token);
-    console.log("token", token);
 
-    console.log(data, isLoading, error);
+    const {data: categories, isLoading, error} = useGetCategoriesQuery(token);
+
+    console.log("data", categories);
+    console.log("error", error);
 
     return (
-        <SafeAreaProvider>
-            <SafeAreaView className="flex-1">
-                <KeyboardAvoidingView
-                    behavior={Platform.OS === "ios" ? "padding" : "height"}
-                    className="flex-1"
-                >
-                    <ScrollView
-                        contentContainerStyle={{flexGrow: 1, paddingHorizontal: 20}}
-                        keyboardShouldPersistTaps="handled"
-                    >
-                        <View
-                            className="w-full flex justify-center items-start my-6"
-                            style={{
-                                minHeight: Dimensions.get("window").height - 100,
-                            }}
-                        >
-                            <Text>Завантаження даних...</Text>
+        <View>
+            <Text style={styles.title}>Категорії</Text>
+            <LoadingOverlay visible={isLoading} />
+            {categories && (
+                <FlatList
+                    data={categories}
+                    keyExtractor={(item) => item.id.toString()}
+                    contentContainerStyle={{ gap: 10, paddingBottom: 200 }}
+                    columnWrapperStyle={{ justifyContent: "space-between" }}
+                    showsVerticalScrollIndicator={false}
+                    numColumns={2}
+                    renderItem={({ item }) => (
+                        <View className="w-[49%] pb-5">
+                            <CategoryCard category={item} />
                         </View>
-                    </ScrollView>
-                </KeyboardAvoidingView>
-            </SafeAreaView>
-        </SafeAreaProvider>
+                    )}
+                />
+            )}
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {flex: 1, justifyContent: "center", padding: 20},
-    title: {fontSize: 20, marginBottom: 15, fontWeight: "bold"},
-    logoutButton: {
-        marginTop: 30,
-        padding: 12,
-        backgroundColor: "#ff4d4d",
-        borderRadius: 8,
-        alignItems: "center",
-    },
-    logoutText: {color: "white", fontWeight: "bold"},
+
+    title: {fontSize: 20, marginBottom: 15, marginTop: 50, textAlign: "center", fontWeight: "bold"},
+
 });
 
 export default CategoriesScreen;
